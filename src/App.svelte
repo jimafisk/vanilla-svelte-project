dc6f4bba5b77500d625f922b1de0fb5d5d21855a
jimafisk <jimafisk@gmail.com>
2019-12-28 13:34:49 -0500

    Replace html.

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,26 @@
 	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<html lang="en">
+	<head>
+		<meta charset='utf-8'>
+		<meta name='viewport' content='width=device-width,initial-scale=1'>
+
+		<title>Svelte app</title>
+
+		<link rel='icon' type='image/png' href='/favicon.png'>
+		<link rel='stylesheet' href='/global.css'>
+		<link rel='stylesheet' href='/build/bundle.css'>
+
+		<script defer src='/build/bundle.js'></script>
+	</head>
+	<body class="hot">
+	<main>
+		<h1>Hello {name}!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</main>
+	</body>
+</html>
 
 <style>
 	main {
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import App from './App.svelte';
 
-const app = new App({
-	target: document.body,
+const replaceContainer = function ( Component, options ) {
+    const frag = document.createDocumentFragment();
+    const component = new Component( Object.assign( {}, options, { target: frag } ));
+
+    options.target.replaceWith( frag );
+
+    return component;
+}
+
+// const app = new App({
+  const app = replaceContainer( App, {
+	//target: document.html,
+	target: document.querySelector( '#target' ),
 	props: {
 		name: 'world'
 	}
