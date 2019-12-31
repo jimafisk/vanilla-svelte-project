4e9abe760455f6aaef5e6b735f2b30bc7197db78
jimafisk <jimafisk@gmail.com>
2019-12-31 18:21:36 -0500

    Remove wrapping html from individual pages.

--- src/App.svelte
+++ src/App.svelte
@@ -8,6 +8,24 @@
 	</body>
 </html>
 
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+	:global(body) {
+		padding: 0;
+		margin: 0;
+	}
+</style>
+
 <script>
 	import Navaid from 'navaid';
 	import { onDestroy } from 'svelte';
--- src/routed/About.svelte
+++ src/routed/About.svelte
@@ -1,36 +1,10 @@
 <script>
-    import Head from '../Head.svelte';
-    import Nav from '../Nav.svelte';
 </script>
 
-<html lang="en">
-	<Head />
-	<body class="hot">
-	<Nav />
-	<main>
-		<h1>About Svelte Component!</h1>
-	</main>
-	</body>
-</html>
+<h1>About Svelte Component!</h1>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		color: blue;
 	}
 </style>
